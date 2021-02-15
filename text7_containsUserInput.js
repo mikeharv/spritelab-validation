f873a0e48865ef9a92ced9bbe18b1f23364742a9
Mike Harvey <43474485+mikeharv@users.noreply.github.com>
2021-02-15 15:55:39 -0500

    Create text7_containsUserInput.js

---
+++ text7_containsUserInput.js
@@ -0,0 +1,104 @@
+var spriteIds = getSpriteIdsInUse();
+// Fail right away if they don't have any sprites
+if (spriteIds.length < 1) {
+  levelFailure(3, 'noSprites');
+}
+
+if (!validationProps.previous) {
+  validationProps.previous = {}; // id => {logLength: 1}
+}
+
+if (!validationProps.successCriteria) {
+  validationProps.successCriteria = {
+    madePrompt: false,
+    answeredPrompt: false,
+    printedText: false,
+    logIncreased: false,
+    containsInput: false
+  };
+}
+
+var allPrompts = getPromptVars();
+var promptVars = Object.keys(allPrompts);
+var printLog = getPrintLog();
+var failTime = 30;
+
+//check if they made any prompts
+if(!validationProps.successCriteria.madePrompt){
+  validationProps.successCriteria.madePrompt = promptVars.length>=1;
+} else{
+  //bump up the fail time if they did
+  failTime = 300;
+  //check for a non-null value in any prompt variable
+  for (var prop in promptVars) {
+    if(allPrompts[promptVars[prop]]!= null){
+      //console.log(allPrompts[promptVars[prop]]);
+      validationProps.successCriteria.answeredPrompt = true;
+    }
+  }
+}
+
+//any printed text?
+if(printLog.length){
+  validationProps.successCriteria.printedText=true;
+  //more than last frame?
+  if(printLog.length>validationProps.previous.logLength){
+    validationProps.successCriteria.logIncreased=true;
+  }
+  validationProps.previous = {
+    logLength: printLog.length
+  };
+}
+
+//go through all the printed text and all prompts to see if they ever printed the text entered
+for (var i = 0; i < printLog.length; i++) {
+  for (var prop in promptVars) {
+    var enteredText=allPrompts[promptVars[prop]];
+    var printedText=printLog[i];
+    if(printedText.includes(enteredText)){
+      validationProps.successCriteria.containsInput = true;
+    }
+  }
+} 
+
+//Set the successTime the first time they've passed the two main criteria
+if(validationProps.successCriteria.answeredPrompt&&validationProps.successCriteria.containsInput&&!validationProps.successTime){
+  console.log(World.frameCount);
+  validationProps.successTime=World.frameCount;
+}
+
+
+if (!validationProps.successTime && World.frameCount > failTime) {
+  if (!validationProps.successCriteria.madePrompt) {
+    console.log('didntmakeprompt');
+    levelFailure(3, 'topLeft');
+  } else if (!validationProps.successCriteria.answeredPrompt) {
+    console.log('did not answer prompt');
+    levelFailure(3, 'topRight');
+  } else if (!validationProps.successCriteria.printedText) {
+    console.log('did not print');
+    levelFailure(3, 'bottomLeft');
+  } else if (!validationProps.successCriteria.logIncreased) {
+    console.log('didnt use event');
+    levelFailure(3, 'debugEvents');
+  } else if (!validationProps.successCriteria.containsInput) {
+    console.log('did not print user text');
+    levelFailure(3, 'bottomRight');
+  }
+}
+
+var waitTime = 100;
+if (World.frameCount - validationProps.successTime >= waitTime) {
+  console.log('Generic success');
+  levelFailure(0, 'genericExplore');
+}
+
+push();
+if(!validationProps.successTime){
+  fill(rgb(118,102,160));
+  rect(0,390,(World.frameCount*400/failTime),10);
+} else {
+   fill(rgb(0,173,188));
+  rect(0,390,((World.frameCount-validationProps.successTime)*400/waitTime),10);
+}
+pop();
