77f1fef61e10c6dbf3a0899cd6b1ce25f0cc9a9a
Mike Harvey <43474485+mikeharv@users.noreply.github.com>
2021-06-28 13:27:43 -0400

    Create 1-2.js

---
+++ Spring 2021/Story Board/1-2.js
@@ -0,0 +1,34 @@
+//Story-board - working as needed
+
+if (!validationProps.successCriteria) {
+  validationProps.successCriteria = {
+    twoSprites: false,
+    differentLocations: false
+  };
+}
+
+//Helper variables
+var spriteIds = getSpriteIdsInUse();
+var animations = getAnimationsInUse();
+
+// Count Sprites
+if(!validationProps.successCriteria.twoSprites){
+  validationProps.successCriteria.twoSprites = spriteIds.length >= 2;
+}
+
+//Check sprite locations
+if(validationProps.successCriteria.twoSprites){
+  if (getProp({id: spriteIds[0]}, "x")!=getProp({id: spriteIds[1]}, "x") || getProp({id: spriteIds[0]}, "y")!=getProp({id: spriteIds[1]}, "y")) {
+    validationProps.successCriteria.differentLocations=true;
+  }
+}
+
+
+  // Check criteria and give failure feedback
+if (!validationProps.successCriteria.twoSprites) {
+  levelFailure(3, "moreSprites");
+} else if (!validationProps.successCriteria.differentLocations) {
+  levelFailure(3, "changeLocation");
+} else {
+  levelFailure(0, "genericExplore");
+}
