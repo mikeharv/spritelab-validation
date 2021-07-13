91d4ef2cdec7bff68db123286b8a17216e02e63b
Mike Harvey <43474485+mikeharv@users.noreply.github.com>
2021-07-13 10:32:15 -0400

    Update 1-1.js

--- Spring 2021/Story Board/1-1.js
+++ Spring 2021/Story Board/1-1.js
@@ -1,8 +1,6 @@
-//Story board - working as needed.
-
 if (!validationProps.successCriteria) {
   validationProps.successCriteria = {
-    madeSprites: false,
+    starterSprite: false,
   };
 }
 
@@ -10,13 +8,38 @@ if (!validationProps.successCriteria) {
 var spriteIds = getSpriteIdsInUse();
 
 // Count Sprites
-if(!validationProps.successCriteria.madeSprites){
-  validationProps.successCriteria.madeSprites = (spriteIds.length >= 1);
+validationProps.successCriteria.starterSprite = (spriteIds.length>=1);
+
+// Set success time if success
+if (validationProps.successCriteria.starterSprite &&
+   !validationProps.successTime)
+{
+  validationProps.successTime = World.frameCount;
 }
 
+var failTime = 150;
+
 // Check criteria and give failure feedback
-if (!validationProps.successCriteria.madeSprites) {
-  levelFailure(3, "noSprites");
-} else {
+if (World.frameCount > failTime) {
+  if (!validationProps.successCriteria.starterSprite) {
+    levelFailure(3, "noSprites");
+  } 
+}
+
+// Pass 60 frames after success
+var waitTime = 60;
+if (World.frameCount - validationProps.successTime >= waitTime) {
+  console.log("Generic success");
   levelFailure(0, "genericSuccess");
 }
+
+push();
+stroke("white");
+if (!validationProps.successCriteria.starterSprite) {
+  fill(rgb(118,102,160));
+  rect(0,390,(World.frameCount*400/failTime),10);
+} else {
+  fill(rgb(0,173,188));
+  rect(0,390,((World.frameCount-validationProps.successTime)*400/waitTime),10);
+}
+pop();
