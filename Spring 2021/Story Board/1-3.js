ca2e5b4f4fcbb30404180c92e47d95c6ae3c437c
Mike Harvey <43474485+mikeharv@users.noreply.github.com>
2021-07-13 10:35:40 -0400

    Update 1-3.js

--- Spring 2021/Story Board/1-3.js
+++ Spring 2021/Story Board/1-3.js
@@ -1,54 +1,69 @@
-var spriteIds = getSpriteIdsInUse();
-// Fail right away if they don't have any sprites
-if (spriteIds.length < 1) {
-  levelFailure(3, 'noSprites');
-}
-
 if (!validationProps.successCriteria) {
   validationProps.successCriteria = {
-    usedSpeech: false
+    twoSprites: false,
+    differentLocations: false,
+    differentCostumes: false
   };
 }
 
+//Helper variables
 var spriteIds = getSpriteIdsInUse();
 
-for (var i = 0; i < spriteIds.length; i++) {
-  if(getProp({ id: i }, "speech")){  
-    validationProps.successCriteria.usedSpeech = true;
-  }
-  if(getProp({ id: i }, "timeOut")){  
-    validationProps.activeSpeech = true;
+//Count Sprites
+validationProps.successCriteria.twoSprites = spriteIds.length>=2;
+
+//Check sprite locations
+if(validationProps.successCriteria.twoSprites){
+  if (getProp({id: spriteIds[0]}, "x")!=getProp({id: spriteIds[1]}, "x") || getProp({id: spriteIds[0]}, "y")!=getProp({id: spriteIds[1]}, "y")) {
+    validationProps.successCriteria.differentLocations=true;
   }
 }
 
+//Check sprite costumes
+if(validationProps.successCriteria.twoSprites){
+  if (getProp({id: spriteIds[0]}, "costume")!=getProp({id: spriteIds[1]}, "costume")) {
+    validationProps.successCriteria.differentCostumes=true;
+  }
+}
 
-// Check if this is the first frame that meets the success criteria
-if (!validationProps.successTime&&validationProps.successCriteria.usedSpeech) {
+// Set success time if success
+if (validationProps.successCriteria.twoSprites &&
+    validationProps.successCriteria.differentLocations &&
+    validationProps.successCriteria.differentCostumes &&
+   !validationProps.successTime)
+{
   validationProps.successTime = World.frameCount;
-  //console.log(validationProps.successTime);
 }
 
-var failTime = 30;
-if(World.frameCount > failTime){
-  if(!validationProps.successCriteria.usedSpeech){
-    console.log("Did not use speech");
-    levelFailure(3, 'addPrintBlock');
-  } else {
-    console.log(validationProps);
+// Delay fail time (so student can observe the wrong animation)
+var failTime = 10;
+
+//Check criteria and give failure feedback
+if (World.frameCount > failTime) {
+  if (!validationProps.successCriteria.twoSprites) {
+    levelFailure(3, "createAtLeastTwoSprites");
+  } else if (!validationProps.successCriteria.differentLocations) {
+    levelFailure(3, "changeLocation");
+  } else if (!validationProps.successCriteria.differentCostumes) {
+    levelFailure(3, "spritesNeedUniqueCostumes");
   }
 }
-var waitTime = 130;
-if ((World.frameCount - validationProps.successTime >= waitTime) && !validationProps.activeSpeech) {
-  console.log('Generic success');
-  levelFailure(0, 'genericSuccess');
+
+// Pass 60 frames after success
+var waitTime = 60;
+if (World.frameCount - validationProps.successTime >= waitTime) {
+  levelFailure(0, "genericExplore");
 }
 
 push();
-if(!validationProps.successTime){
-  fill(rgb(118, 102, 160));
-  rect(0, 390, (World.frameCount * 400) / failTime, 10);
+stroke("white");
+if (!validationProps.successCriteria.twoSprites ||
+    !validationProps.successCriteria.differentLocations ||
+    !validationProps.successCriteria.differentCostumes) {
+fill(rgb(118,102,160));
+rect(0,390,(World.frameCount*400/failTime),10);
 } else {
-   fill(rgb(0,173,188));
-  rect(0,390,((World.frameCount-validationProps.successTime)*400/waitTime),10);
+fill(rgb(0,173,188));
+rect(0,390,((World.frameCount-validationProps.successTime)*400/waitTime),10);
 }
 pop();
